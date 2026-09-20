7f002776fe2874bc3b96d100b94d667c5b8e146b
awasl <awasl@users.noreply.github.com>
2026-09-20 14:16:22 +0800

    Merge: 同步 LmeSzinc/AzurLaneAutoScript master，并保留 Dashboard 额外改动
    Keep dashboard UI, resource logging, and related assets while updating to upstream 74e8231.

--- module/coalition/coalition.py
+++ module/coalition/coalition.py
@@ -6,6 +6,7 @@ from module.coalition.combat import CoalitionCombat
 from module.exception import ScriptEnd, ScriptError
 from module.logger import logger
 from module.ocr.ocr import Digit
+from  module.log_res.log_res import LogRes
 from module.ui.page import page_campaign_menu
 
 
@@ -75,6 +76,8 @@ class Coalition(CoalitionCombat, CampaignEvent):
             pt = ocr.ocr(self.device.image)
             # 999999 seems to be a default value, wait
             if pt not in [999999]:
+                LogRes(self.config).Pt = pt
+                self.config.update()
                 break
         else:
             logger.warning('Wait PT timeout, assume it is')
